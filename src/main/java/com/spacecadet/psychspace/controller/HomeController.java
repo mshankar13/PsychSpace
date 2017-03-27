61a9cfcb9f466ef72c7275e8e1cd67bba38a7dbc
zombiecircus <liao840305@gmail.com>
2017-03-27 17:13:57 -0400

    before pull

--- src/main/java/com/spacecadet/psychspace/controller/HomeController.java
+++ src/main/java/com/spacecadet/psychspace/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.spacecadet.psychspace.controller;
 
+import com.spacecadet.psychspace.dataManager.UserManager;
+import com.spacecadet.psychspace.utilities.AuthenticateUserRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,6 +13,9 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class HomeController {
+
+    private UserManager userManager = new UserManager();
+
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public ModelAndView home(){
         ModelAndView model = new ModelAndView();
@@ -17,4 +23,16 @@ public class HomeController {
 
         return model;
     }
+
+    @RequestMapping(value = "/home", method = RequestMethod.POST)
+    public ModelAndView afterLogin(@RequestBody AuthenticateUserRequest request){
+        ModelAndView model = new ModelAndView();
+        model.setViewName("welcome");
+        if (userManager.verifyUser(request.getUsername(), request.getPassword())){
+            model.setViewName("home");
+        }
+
+        return model;
+    }
+
 }
