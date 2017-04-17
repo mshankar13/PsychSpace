32392bc9edefac0c4a751d27868c864d63350e53
Andrea Cerini <amcerini@gmail.com>
2017-04-17 16:25:37 -0400

    Course box resizing for large titles on catalogue

--- src/main/webapp/WEB-INF/resources/js/style.js
+++ src/main/webapp/WEB-INF/resources/js/style.js
@@ -1,18 +1,46 @@
 $(document).ready(function () {
-    $(".ps-text-content").each(function () { //loop through each first
-
-        var largest = 0;
-
-        $(this).find(".ps-item").each(function () { //find each .titlebox within its parent (rest is the same)
-            var findHeight = $(this).height();
-            if (findHeight > largest) {
-                largest = findHeight;
-            }
-        });
-
-        $(this).find(".").css({
-            "height": largest + "px"
-        }); //update all .titlebox inside of this ul
-
-    });
+    $(window).resize();
 });
+
+$(window).resize('resize', function (event) {
+    //  if ($(window).width() > 720) {
+    scaleBoxes();
+    ///  }
+});
+
+function scaleBoxes() {
+    var largest_col = 0;
+    var largest_well = 0;
+
+    $(document).find(".col-lg-4").each(function () { //find each .titlebox within its parent (rest is the same)
+        var findHeight = $(this).height();
+        if (findHeight > largest_col) {
+            largest_col = findHeight;
+        }
+    });
+
+    $(document).find(".col-lg-4").css({
+        "height": largest_col + "px",
+        "margin-bottom": "1em"
+    }); //update all .titlebox inside of this ul
+
+    $(document).find(".ps-well").css({
+        "height": largest_col + "px",
+        "margin-bottom": "1em"
+    }); //update all .titlebox inside of this ul
+
+    $(document).find("h2").each(function () { //find each .titlebox within its parent (rest is the same)
+        var findHeight = $(this).height();
+        if (findHeight > largest_well) {
+            largest_well = findHeight;
+        }
+    });
+
+    $(document).find("h2").css({
+        "height": largest_well + "px",
+        "display": "table-cell",
+        "vertical-align": "bottom",
+        "width": "100vw"
+    });
+
+}
