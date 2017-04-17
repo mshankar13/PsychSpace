aa0bf46c86fddf3217f5bcd7c99344d36f4c7c0e
Andrea Cerini <amcerini@gmail.com>
2017-04-17 12:22:00 -0400

    UI for Video in Learn page
    Working on making all boxes for news/catalogue to be the same height
    when the title is long

--- src/main/webapp/WEB-INF/resources/js/style.js
+++ src/main/webapp/WEB-INF/resources/js/style.js
@@ -1,16 +1,18 @@
-$("ul").each(function () { //loop through each first
+$(document).ready(function () {
+    $(".ps-text-content").each(function () { //loop through each first
 
-    var largest = 0;
+        var largest = 0;
 
-    $(this).find(".titlebox").each(function () { //find each .titlebox within its parent (rest is the same)
-        var findHeight = $(this).height();
-        if (findHeight > largest) {
-            largest = findHeight;
-        }
+        $(this).find(".ps-item").each(function () { //find each .titlebox within its parent (rest is the same)
+            var findHeight = $(this).height();
+            if (findHeight > largest) {
+                largest = findHeight;
+            }
+        });
+
+        $(this).find(".").css({
+            "height": largest + "px"
+        }); //update all .titlebox inside of this ul
+
     });
-
-    $(this).find(".").css({
-        "height": largest + "px"
-    }); //update all .titlebox inside of this ul
-
 });
