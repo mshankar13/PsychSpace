f262b941f473973f5d623a596273a8d3e04848f7
CelesteM <celeste.q.ma@hotmail.com>
2017-05-01 20:15:14 -0400

    commented out new attribute score for course

--- src/main/java/com/spacecadet/psychspace/testData/CatalogTest.java
+++ src/main/java/com/spacecadet/psychspace/testData/CatalogTest.java
@@ -16,14 +16,14 @@ public class CatalogTest {
      */
     public void course_test() {
         // Before use add user keys to first field of each call!
-        Course course1 = new Course("", "", "Exam Time Management", "Bob", "this is a course",
-                "3/3/17", "4/3/17", "3/2/17", "4/3/17", "closed", "20", "25");
-        Course course2 = new Course("","", "Homework Time Management", "Angela", "this is a course",
-                "4/3/17", "5/3/17", "4/3/17", "4/2/17", "open", "25", "30");
-        Course course3 = new Course("", "","Application Time Management", "Celeste", "this is a course",
-                "4/16/17", "5/30/17", "4/16/17", "5/2/17", "closed", "25", "25");
-        courseManager.addCourse(course1);
-        courseManager.addCourse(course2);
-        courseManager.addCourse(course3);
+//        Course course1 = new Course("", "", "Exam Time Management", "Bob", "this is a course",
+//                "3/3/17", "4/3/17", "3/2/17", "4/3/17", "closed", "20", "25");
+//        Course course2 = new Course("","", "Homework Time Management", "Angela", "this is a course",
+//                "4/3/17", "5/3/17", "4/3/17", "4/2/17", "open", "25", "30");
+//        Course course3 = new Course("", "","Application Time Management", "Celeste", "this is a course",
+//                "4/16/17", "5/30/17", "4/16/17", "5/2/17", "closed", "25", "25");
+//        courseManager.addCourse(course1);
+//        courseManager.addCourse(course2);
+//        courseManager.addCourse(course3);
     }
 }
